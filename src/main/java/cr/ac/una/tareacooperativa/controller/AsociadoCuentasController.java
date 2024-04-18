9a9f5d83391e928929e155f46c40a6ebc7772027
Stiward Araya <StiwardArayaCalderon98@gmail.com>
2024-04-17 22:55:35 -0600

    MantenimientoCuentas completely working, Acercade button added to main windows

--- src/main/java/cr/ac/una/tareacooperativa/controller/AsociadoCuentasController.java
+++ src/main/java/cr/ac/una/tareacooperativa/controller/AsociadoCuentasController.java
@@ -1,11 +1,17 @@
 package cr.ac.una.tareacooperativa.controller;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+
+import cr.ac.una.tareacooperativa.model.*;
+import cr.ac.una.tareacooperativa.util.AppContext;
+import cr.ac.una.tareacooperativa.util.Mensaje;
 import io.github.palexdev.materialfx.controls.MFXComboBox;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 
 /**
  * <p>
@@ -17,6 +23,10 @@ import javafx.fxml.Initializable;
  */
 public class AsociadoCuentasController extends Controller implements Initializable {
 
+    RegistroAsociado registroAsociado;
+    RegistroAsociadoCuenta registroAsociadoCuenta;
+    RegistroCuenta registroCuenta;
+
     @javafx.fxml.FXML
     private MFXComboBox mcbTipoCuenta;
     @javafx.fxml.FXML
@@ -27,16 +37,38 @@ public class AsociadoCuentasController extends Controller implements Initializab
     private MFXTextField txtfTipoCuenta;
 
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-    }
+    public void initialize(URL url, ResourceBundle rb) {}
 
     @Override
     public void initialize() {
+        txtfSaldo.setEditable(false);
+        txtfTipoCuenta.setEditable(false);
+        cargarRegistros();
     }
 
     @javafx.fxml.FXML
     public void onActionBtnBuscar(ActionEvent actionEvent) {
-        //TODO registroAsociado.buscarAsociado((txtfFolio.getText()).toUpperCase());->
+        if(txtfFolio.getText().isEmpty()){
+            new Mensaje().showModal(Alert.AlertType.ERROR, "Error de usuario", getStage(), "Debes ingresar un Folio");
+        }else if(registroAsociado.buscarAsociado(txtfFolio.getText()) == null){
+            new Mensaje().showModal(Alert.AlertType.ERROR, "Error de usuario", getStage(), "Usuario no encontrado");
+        }else{
+            ArrayList<AsociadoCuenta> asociadosCuentas = registroAsociadoCuenta.getAsociadosCuentas();
+            for (AsociadoCuenta asoCu : asociadosCuentas){
+                if(asoCu.getFolioAsociado().equals(txtfFolio.getText())){
+                    mcbTipoCuenta.getItems().add(registroCuenta.buscarCuenta(asoCu.getIdCuenta()).getNombre());
+                }
+            }
+            new Mensaje().showModal(Alert.AlertType.CONFIRMATION, "Cuentas", getStage(), "Cuentas cargadas");
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void onActionBtnVer(ActionEvent actionEvent) {
+        Cuenta cuenta = registroCuenta.buscarCuentaNombre(mcbTipoCuenta.getSelectedItem().toString());
+        AsociadoCuenta asoCu = registroAsociadoCuenta.buscarAsociadoCuenta(txtfFolio.getText(), cuenta.getId());
+        txtfTipoCuenta.setText(cuenta.getNombre());
+        txtfSaldo.setText(String.valueOf(asoCu.getBalanceCuenta()));
     }
 
     @javafx.fxml.FXML
@@ -44,5 +76,17 @@ public class AsociadoCuentasController extends Controller implements Initializab
         txtfFolio.setText("");
         txtfSaldo.setText("");
         txtfTipoCuenta.setText("");
+        mcbTipoCuenta.getItems().clear();
     }
+
+    private void cargarRegistros(){
+        registroAsociado = ((RegistroAsociado) AppContext.getInstance().get("asociados"));
+        registroAsociadoCuenta = ((RegistroAsociadoCuenta) AppContext.getInstance().get("asociadosCuentas"));
+        registroCuenta = ((RegistroCuenta) AppContext.getInstance().get("cuentas"));
+        registroAsociado.cargarAsociados();
+        registroAsociadoCuenta.cargarAsociadoCuenta();
+        registroCuenta.cargarCuentas();
+    }
+
+
 }
